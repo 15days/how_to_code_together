fc67673fa450cbf98b0c3ae11223228dc9ac1bb5
Myeongjin <mjleez95@gmail.com>
2019-01-29 20:37:21 +0900

    test

--- exercise/remainOddOrEven.py
+++ exercise/remainOddOrEven.py
@@ -1,11 +1,16 @@
 def remainOddOrEven(int_list):
     """Delete odd or even int by length of list
-    
+
     Arguments:
         int_list {[list of int]} -- [outlier deleted list]
-    
+
     Returns:
         [list of int] -- [only odd or even ints]
     """
-    
+
+    if len(int_list) % 2 == 0:
+        modified_list = [int for int in int_list if int % 2 == 0]
+    else:
+        modified_list = [int for int in int_list if int % 2 != 0]
+
     return modified_list
